5056648972134178d2697b9507ebb17b787b1339
moun <chal2andev@gmail.com>
2026-02-18 00:24:01 +0900

    url parsing

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#web scraping script
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.naver.com'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print(soup.select('a'))
